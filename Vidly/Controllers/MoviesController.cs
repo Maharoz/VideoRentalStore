f0f3aef2983b1e48aff89406705b37581cfbb4a1
maharoz <mahruj66@gmail.com>
2019-02-21 20:36:23 +0600

    movie added and edited

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -30,14 +30,36 @@ namespace Vidly.Controllers
             return View(movies);
         }
 
-        //private IEnumerable<Movie> GetMovies()
-        //{
-        //    return new List<Movie>
-        //    {
-        //        new Movie { Id = 1, Name = "Shrek" },
-        //        new Movie { Id = 2, Name = "Wall-e" }
-        //    };
-        //}
+
+        public ViewResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+
 
         public ActionResult Details(int id)
         {
@@ -70,26 +92,30 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
-        //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
-        //public ActionResult ByReleaseDate(int year , int month)
-        //{
-        //    return Content(year + "/" + month);
-        //}
 
-        //public ActionResult Edit(int id)
-        //{
-        //    return Content("id=" + id);
-        //}
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
 
-        ////Movies
-        //public ActionResult Index(int? pageIndex , string sortBy)
-        //{
-        //    if (!pageIndex.HasValue)
-        //        pageIndex = 1;
-        //    if (string.IsNullOrWhiteSpace(sortBy))
-        //        sortBy = "Name";
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
 
-        //    return Content(String.Format("PageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        //}
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Movies");
+        }
+
+
+
     }
 }
