38dab9d5029353ad5bd4781bcee078f374c110c8
maharoz <mahruj66@gmail.com>
2019-02-21 14:26:14 +0600

    Dropdown added

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -25,7 +25,12 @@ namespace Vidly.Controllers
 
         public ActionResult New()
         {
-            return View();
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes =  membershipTypes
+            };
+            return View(viewModel);
         }
         public ViewResult Index()
         {
